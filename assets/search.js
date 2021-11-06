3e79ba1e72263dbf03f783f404da462ed142c1b3
José Manuel Martínez <josemanu.infor@gmail.com>
2021-11-06 21:36:13 +0100

    first commit

---
+++ assets/search.js
@@ -0,0 +1,1 @@
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Example protected page",'url':"/2021/06/11/protected-post-example.html"}]};
